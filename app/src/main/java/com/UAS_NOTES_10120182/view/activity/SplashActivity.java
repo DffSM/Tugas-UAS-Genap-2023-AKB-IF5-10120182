bd7bcdd9ddee39fd01cf3a72d3ca58535d9eaad9
Daffa <suryadaffa30@gmail.com>
2023-08-12 18:48:09 +0700

    UAS-AKB

---
+++ app/src/main/java/com/UAS_NOTES_10120182/view/activity/SplashActivity.java
@@ -0,0 +1,35 @@
+package com.UAS_NOTES_10120182.view.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.UAS_NOTES_10120182.R;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_splash);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+            }
+        }, 3000L);
+    }
+}
+
+/// 10120182, Daffa Surya Mahardhika, IF-5
